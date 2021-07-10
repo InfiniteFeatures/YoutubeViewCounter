3d2bff5e4e51b35936a0e9c34a175e926c3fcca3
InfiniteFeatures <laura.lavigueur@outlook.com>
2021-07-10 15:19:55 -0400

    Changed background.js indentation from 2 to 4

--- background.js
+++ background.js
@@ -1,14 +1,14 @@
 console.log("background here");
 
 chrome.tabs.onUpdated.addListener(
-  function (tabId, changeInfo, tab) {
-    console.log("maybe sending message", changeInfo);
-    if (changeInfo.url && changeInfo.url.match(/youtube.com\/watch\?/)) {
-      console.log("sending message");
-      chrome.tabs.sendMessage(tabId, {
-        message: 'video',
-        url: changeInfo.url
-      })
+    function (tabId, changeInfo, tab) {
+        console.log("maybe sending message", changeInfo);
+        if (changeInfo.url && changeInfo.url.match(/youtube.com\/watch\?/)) {
+            console.log("sending message");
+            chrome.tabs.sendMessage(tabId, {
+                message: 'video',
+                url: changeInfo.url
+            })
+        }
     }
-  }
 );
